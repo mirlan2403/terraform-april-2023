9dc4d5c405ca8e571e54a70da80ff7c3bf9ca9d5
mirlan2403 <achilovmirlan@gmail.com>
2023-06-27 01:28:28 +0000

    added Class5 files

---
+++ Class5-EC2/main.tf
@@ -0,0 +1,33 @@
+resource "aws_instance" "web" {
+  ami           = "ami-0e75dc68421586157"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  
+    ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+    ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
